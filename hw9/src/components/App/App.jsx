e0c61353c227478ac181828490cb29aea5513316
Dmitrii <dmitrii.osadcii1997@gmail.com>
2025-04-02 17:55:21 +0300

    hw 9

---
+++ hw9/src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import DynamicForm from '../DynamicForm/DynamicForm'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <DynamicForm />
+
+      
+    </>
+
+  )
+}
+
+export default App
---
+++ hw9/src/components/DynamicForm/DynamicForm.jsx
@@ -0,0 +1,61 @@
+import styles from './DynamicForm.module.css'
+import { useForm } from 'react-hook-form';
+
+const DynamicForm = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+        mode: "onBlur"
+    });
+
+    const onSubmit = () => {
+
+    }
+    const name = watch('name');
+
+    return (
+        <>
+            {name ? (
+                <form className={styles.main} onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <h2>Введите логин</h2>
+                        <input
+                            {...register('name', { required: 'Ввод поля обязателен', minLength: { value: 5, message: 'Минимум 5 символов' } })}
+                            type="text"
+                        />
+                        {errors.name && <p>{errors.name.message}</p>}
+                    </div>
+                    <div>
+                        <h3>Введите E-mail</h3>
+                        <input
+                            {...register('email', {
+                                required: 'Ввод поля обязателен',
+                                pattern: {
+                                    value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                                    message: 'Неправильный формат e-mail'
+                                }
+                            })}
+                            type="text"
+                        />
+                        {errors.email && <p>{errors.email.message}</p>}
+                    </div>
+                    <button>Submit</button>
+                </form>
+            ) : (
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <h2>Введите логин</h2>
+                        <input
+                            {...register('name', { required: 'Ввод поля обязателен', minLength: { value: 5, message: 'Минимум 5 символов' } })}
+                            type="text"
+                        />
+                        {errors.name && <p>{errors.name.message}</p>}
+                    </div>
+                    <button>Submit</button>
+                </form>
+            )}
+        </>
+    );
+};
+
+
+export default DynamicForm;
